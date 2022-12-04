7a0dd03e0e3ba37e1aa2c42ce6d7543596de933a
“dennohkim” <kimathidennis38@gmail.com>
2022-12-05 02:46:48 +0300

    feat: sort dictionary object

--- src/my-view3.js
+++ src/my-view3.js
@@ -7,13 +7,46 @@ class MyView3 extends LitElement {
   // Declare the properties of the element
   static get properties() {
     return {
-      todos: { type: Array } // An array of todo items
+      todos: { type: Array }, // An array of todo items
+      sortedObj: { type: Object } // The sorted object
     };
   }
 
   constructor() {
     super();
     this.todos = []; // Initialize the todos property as an empty array
+    const obj = {
+      '34': 'thirty-four',
+      '90': 'ninety',
+      '91': 'ninety-one',
+      '21': 'twenty-one',
+      '61': 'sixty-one',
+      '9': 'nine',
+      '2': 'two',
+      '6': 'six',
+      '3': 'three',
+      '8': 'eight',
+      '80': 'eighty',
+      '81': 'eighty-one',
+      'Ninety-Nine': '99',
+      'nine-hundred': '900'
+    };
+    const sortedObj = {};
+
+    Object.keys(obj).sort((a, b) => {
+    // Compare the keys as numbers if they are both numeric
+    if (isNaN(a) && isNaN(b)) {
+      return a - b;
+    }
+    // Compare the keys as strings if they are not numeric
+    return a.localeCompare(b);
+  })
+  .forEach(key => {
+    // Add the sorted keys and values to the new object
+    sortedObj[key] = obj[key];
+  });
+
+  console.log(sortedObj); 
   }
 
   render() {
@@ -25,6 +58,7 @@ class MyView3 extends LitElement {
           padding: 10px;
           border: 1px solid #ccc;
         }
+        
       </style>
       <h1>My Todos</h1>
       ${this.todos.map(todo => html`
@@ -33,6 +67,7 @@ class MyView3 extends LitElement {
           <p>Complete: ${todo.completed}</p>
         </div>
       `)}
+
     `;
   }
 
