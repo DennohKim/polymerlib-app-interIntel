dcfee3f9f1b8d80e9622b08bb9a34e60b02d88b3
“dennohkim” <kimathidennis38@gmail.com>
2022-12-05 02:11:03 +0300

    feat: declare properties of the element

--- src/my-view3.js
+++ src/my-view3.js
@@ -4,17 +4,11 @@ import { LitElement, html } from 'lit-element';
 
 
 class MyView3 extends LitElement {
-  render() {
-    return html`
-      
-
-      <div class="card">
-        <div class="circle">3</div>
-        <h1>View Three</h1>
-        <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
-        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
-      </div>
-    `;
+  // Declare the properties of the element
+  static get properties() {
+    return {
+      todos: { type: Array } // An array of todo items
+    };
   }
 
   firstUpdated(changedProperties) {
