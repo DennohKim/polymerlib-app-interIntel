0d4e60e88e2f2849895b7c1af8e0a8ffad3d76ca
“dennohkim” <kimathidennis38@gmail.com>
2022-12-05 02:08:41 +0300

    feat: make response when element is first updated

--- src/my-view3.js
+++ src/my-view3.js
@@ -1,6 +1,32 @@
+
+
 import { LitElement, html } from 'lit-element';
 
+
 class MyView3 extends LitElement {
-  
+  render() {
+    return html`
+      
+
+      <div class="card">
+        <div class="circle">3</div>
+        <h1>View Three</h1>
+        <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
+        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
+      </div>
+    `;
+  }
+
+  firstUpdated(changedProperties) {
+     // Make a request to the specified URL
+     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+     .then(response => response.json()) // Parse the response as JSON
+     .then(data => {
+       // Display the response data in the console
+       console.log(data);
+     });
+    
+  }
 }
+
 customElements.define('my-view3', MyView3);
